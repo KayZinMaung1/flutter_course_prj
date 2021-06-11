04f1b837482d1bd392ab41b67011ce31a9f4f979
Pyae Sone <pyaesone@Pyaes-MacBook-Air.local>
2021-06-11 22:18:52 +0630

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:first_app/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: myTheme,
+      debugShowCheckedModeBanner: false,
+      home: IncrementWidget()
+    );
+  }
+}
+
+class IncrementWidget extends StatefulWidget {
+
+  @override
+  _IncrementWidgetState createState() => _IncrementWidgetState();
+}
+
+class _IncrementWidgetState extends State<IncrementWidget> {
+
+  int number = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState(() {
+            number++;
+          });
+        },
+        child: Icon(Icons.add)
+      ),
+
+      appBar: AppBar(
+        title: Text("Increment"),
+      ),
+      body: Container(
+        //padding: EdgeInsets.all(50),
+        //margin: EdgeInsets.all(50),
+        //margin: EdgeInsets.only(left:20,right:30,top:20,bottom:50),
+        //margin:EdgeInsets.symmetric(vertical:50,horizontal:30),
+        color: Color(0xfff5c4948),
+        width: 300,
+        height: 300,
+        child: Center(
+          child: Text(number.toString(),style: TextStyle(
+            fontSize: 18.5,
+            color: Colors.white
+          ),)
+        )
+      ),
+
+    );
+  }
+}
---
+++ lib/theme/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+ThemeData myTheme = ThemeData(
+    primaryColor: Colors.green,
+    accentColor: Colors.red,
+);
